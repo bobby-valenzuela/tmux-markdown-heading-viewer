1bfe4bba5f2fdba221eee5fd3e08d31d4f4ffbf2
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2026-03-06 10:00:27 -0800

    reverse layout

--- bin/tmux-markdown-heading-viewer.sh
+++ bin/tmux-markdown-heading-viewer.sh
@@ -11,9 +11,9 @@ else
 
     MARKDOWN_DIR=$(
         if command -v fd >/dev/null && command -v tree >/dev/null; then
-            fd --type d --hidden --follow --exclude .git . /home/ 2>/dev/null | fzf --list=reverse --preview 'tree -C {}' --header 'Choose a folder' --preview-window=down:40%
+            fd --type d --hidden --follow --exclude .git . /home/ 2>/dev/null | fzf --layout=reverse --preview 'tree -C {}' --header 'Choose a folder' --preview-window=down:40%
         else
-            find /home/ -mindepth 1 -type d -not -path '*/\.*' 2>/dev/null | fzf --list=reverse --preview 'ls -la --color=always {}' --preview-window=down:40% --header 'Choose a folder'
+            find /home/ -mindepth 1 -type d -not -path '*/\.*' 2>/dev/null | fzf --layout=reverse --preview 'ls -la --color=always {}' --preview-window=down:40% --header 'Choose a folder'
         fi
     )
 
@@ -38,7 +38,7 @@ print_md_section(){
     BASE_FILE=$(basename "${1}")
     MSG="Choose a heading (File: ${BASE_FILE})"
     # HEADING="$(grep -E '^\s{0,3}#+' $1 | sort -k 2 -r | fzf -m --preview "grep -A 100 {} $1 " --preview-window down:50% --header "${MSG}")"
-    HEADING="$(grep -E '^\s{0,3}#+' $1 | fzf --list=reverse -m --preview "grep -A 100 {} $1 " --preview-window down:50% --header "${MSG}")"
+    HEADING="$(grep -E '^\s{0,3}#+' $1 | fzf --layout=reverse -m --preview "grep -A 100 {} $1 " --preview-window down:50% --header "${MSG}")"
 	# print_md_section_from_heading "${HEADING}" "${1}"
 	HEADING="$(echo "${HEADING}" | sed -E 's:\r::' | sed -E 's:\s+$::')"	# Trim trailing newlines and spaces;	
 	sed -nE "/^\s*${HEADING}/,$ p" $1 | glow -s dark -p			# Print everything after the matching heading
@@ -48,7 +48,7 @@ print_md_section(){
 BASE_DIR=$(basename "${MARKDOWN_DIR}")
 MSG="Choose a file (Folder: ${BASE_DIR})"
 if command -v fd >/dev/null; then
-    FILE="$(fd -H --type file -e md -e markdown . ${MARKDOWN_DIR} | fzf --list=reverse --header "${MSG}" --preview-window down:60% --preview 'glow -p --width=80 {} ' )"
+    FILE="$(fd -H --type file -e md -e markdown . ${MARKDOWN_DIR} | fzf --layout=reverse --header "${MSG}" --preview-window down:60% --preview 'glow -p --width=80 {} ' )"
 else
     FILE="$(find $MARKDOWN_DIR -type f \( -iname "*.md" -o -iname "*.markdown" \) | fzf --header "${MSG}" )"
 fi
