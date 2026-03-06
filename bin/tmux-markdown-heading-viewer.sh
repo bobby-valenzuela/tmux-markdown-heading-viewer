1b3c67fb812cfb74de9dd59bfe0e3233b88d41df
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2026-03-05 16:48:21 -0800

    Improved folder picker

--- bin/tmux-markdown-heading-viewer.sh
+++ bin/tmux-markdown-heading-viewer.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
 # Check if a folder is set
-MARKDOWN_DIR=$(fd --type d . /home/ | fzf --preview 'tree -C {}' )
+MARKDOWN_DIR=$(
+    if command -v fd >/dev/null && command -v tree >/dev/null; then
+        fd --type d --hidden --follow --exclude .git . /home/ 2>/dev/null | fzf --preview 'tree -C {}'
+    else
+        find /home/ -mindepth 1 -type d -not -path '*/\.*' 2>/dev/null | fzf --preview 'ls -la --color=always {}' --preview-window=right:40%
+    fi
+)
+
+
+if [[ -z "$MARKDOWN_DIR" ]]; then
+    echo "No 'MARKDOWN_DIR' variable set" && exit
+fi
 
 print_md_section_from_heading(){
 	HEADING="$(echo "${1}" | sed -E 's:\r::' | sed -E 's:\s+$::')"	# Trim trailing newlines and spaces;	
@@ -12,10 +23,6 @@ print_md_section(){
 	HEADING="$(grep -E '^\s{0,3}#+' $1 | sort -k 2 -r | fzf --preview "grep -A 100 {} $1  ")"
 	print_md_section_from_heading "${HEADING}" "${1}"
 }
-
-if [[ -z "$MARKDOWN_DIR" ]]; then
-    echo "No 'MARKDOWN_DIR' variable set" && exit
-fi
 
 FILE="$(find $MARKDOWN_DIR -type f \( -iname "*.md" -o -iname "*.markdown" \) | fzf )"
 
