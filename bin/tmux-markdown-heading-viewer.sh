ba49b851d1d8262f60d991bc65d8ff79d4ff4559
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2026-03-07 01:24:01 -0800

    Fixed escaping issues

--- bin/tmux-markdown-heading-viewer.sh
+++ bin/tmux-markdown-heading-viewer.sh
@@ -44,8 +44,6 @@ print_md_section(){
         MSG="Choose a heading (File: ${BASE_FILE})"
         HEADING="$(grep -E '^\s{0,3}#+' $FILE | fzf $(printf "${FZF_DEFAULT_OPTS}") --layout=reverse -m --preview "grep -A 100 {} $FILE " --preview-window down:50% --header "${MSG}")"
         HEADING="$(echo "${HEADING}" | sed -E 's:\r::' | sed -E 's:\s+$::')"	# Trim trailing newlines and spaces;	
-        # sed -nE "/^\s*${HEADING}/,$ p" $FILE | glow -s dark -p			# Print everything after the matching heading
-        # HEADING=$(printf $HEADING | sed -E 's:\(:\(:g' | sed -E 's:\):\):g')
         sed -n "/^${HEADING}/,$ p" "$FILE" | glow -s dark -p			# Print everything after the matching heading
     else
         glow -s dark -p "$FILE"
